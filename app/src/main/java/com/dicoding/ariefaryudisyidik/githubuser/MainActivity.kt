0a3f107722d570590ff2c416e51cf6b3538f5266
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-05-21 16:02:18 +0700

    Add icon launcher and splash screen

--- app/src/main/java/com/dicoding/ariefaryudisyidik/githubuser/MainActivity.kt
+++ app/src/main/java/com/dicoding/ariefaryudisyidik/githubuser/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.dicoding.ariefaryudisyidik.githubuser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
